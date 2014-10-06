568d3d4b410aacbdb30464d2defb84ae7f791db2
BigBlueHat <byoung@bigbluehat.com>
2014-10-06 16:12:24 -0400

    Improve messages; Handle _id better

--- updates/yaml.js
+++ updates/yaml.js
@@ -1,22 +1,26 @@
 function (doc, req) {
-  if (!doc){
-    if ('id' in req && req['id']){
+  if (!doc) {
+    if ('id' in req && req['id']) {
       // create new document
-      if (req.headers['Content-Type'] == 'text/yaml') {
+      if (req.headers['Content-Type'].split(';')[0] == 'text/yaml') {
         return [
           {'_id': req['id'], 'yaml': req.body},
-          'YAML stored!'
+          'Your new document is ' + req['id']
         ];
       }
       // TODO: handle other input media types
     }
-    // change nothing in database
-    // TODO: provide a textarea form for pasting in YAML
-    return [null, '...I got nothing...'];
+    // no _id in the req object
+    // so we use the uuid
+    // TODO: parse incoming YAML and check for _id
+    return [
+      {'_id': req['uuid'], 'yaml': req.body},
+      'Your new document is ' + req['uuid']
+    ];
   }
 
   if (req.headers['Content-Type'] == 'text/yaml') {
     doc.yaml = req.body;
   }
-  return [doc, 'YAML updated!'];
+  return [doc, 'YAML document `' + req['id'] + '` updated!'];
 }
